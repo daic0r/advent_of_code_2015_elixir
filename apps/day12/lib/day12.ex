16315e72d38b7ba5712431bd3e11bf1219e38cd7
daic0r <daic0r.dev@gmail.com>
2024-10-30 12:19:07 +0100

    Day 12 done

---
+++ apps/day12/lib/day12.ex
@@ -0,0 +1,39 @@
+defmodule Day12 do
+  @part 2
+
+  def process(input, acc) when is_number(input) do
+    input + acc
+  end
+  def process(input, acc) when is_binary(input) do
+    acc
+  end
+  def process(input, acc) when is_list(input) do
+    acc + Enum.reduce(input, 0, fn x, acc ->
+      IO.inspect(x)
+      acc + process(x, 0)
+    end)
+  end
+  def process(input, acc) when is_map(input) and @part == 1 do
+    acc + Enum.reduce(input, 0, fn {_key,val}, acc -> acc + process(val, 0) end)
+  end
+  def process(input, acc) when is_map(input) and @part == 2 do
+    if Enum.any?(input, fn {key, val} -> key == "red" or val == "red" end) do
+      acc
+    else
+      acc + Enum.reduce(input, 0, fn {_key,val}, acc -> acc + process(val, 0) end)
+    end
+  end
+      
+  def main() do
+    input = File.read!("input.txt")
+    input_json = Jason.decode!(input) |> IO.inspect
+    result = process(input_json, 0)
+
+    IO.puts "Part 1 Result = #{result}"
+  end
+
+  def start_link do
+    main()
+    {:ok, self()}
+  end
+end
---
+++ apps/day12/lib/day12/application.ex
@@ -0,0 +1,21 @@
+defmodule Day12.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Day12.Worker.start_link(arg)
+      # {Day12.Worker, arg}
+      %{ id: Day12, start: {Day12, :start_link, []} }
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Day12.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
