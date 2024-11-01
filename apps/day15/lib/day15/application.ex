950172b3053d621b16246971010172c8720f90d4
daic0r <daic0r.dev@gmail.com>
2024-11-01 18:55:03 +0100

    Day 15 Part 1

---
+++ apps/day15/lib/day15/application.ex
@@ -0,0 +1,21 @@
+defmodule Day15.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Day15.Worker.start_link(arg)
+      # {Day15.Worker, arg}
+      %{ id: Day15, start: {Day15, :start_link, []} }
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Day15.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
